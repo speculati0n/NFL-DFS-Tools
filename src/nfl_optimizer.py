5cb86a78264754067a0ecee3848614b56ba32b24
speculati0n <barryanderson407@gmail.com>
2025-08-30 11:04:05 -0400

    Show optimizer progress on main page

--- src/nfl_optimizer.py
+++ src/nfl_optimizer.py
@@ -296,7 +296,7 @@ class NFL_Optimizer:
                     self.player_dict[(player_name, position, team)]
                 )
 
-    def optimize(self):
+    def optimize(self, progress_callback=None):
         # Setup our linear programming equation - https://en.wikipedia.org/wiki/Linear_programming
         # We will use PuLP as our solver - https://coin-or.github.io/pulp/
 
@@ -885,8 +885,13 @@ class NFL_Optimizer:
             fpts_used = self.problem.objective.value()
             self.lineups.append((players, fpts_used))
 
-            if i % 100 == 0:
-                print(i)
+            progress = i + 1
+            display_progress = min(progress, self.num_lineups)
+            percent = (display_progress / self.num_lineups) * 100
+            if progress_callback:
+                progress_callback(display_progress, self.num_lineups)
+            else:
+                print(f"{display_progress}/{self.num_lineups} {percent:.0f}%")
 
             # Ensure this lineup isn't picked again
             self.problem += (
